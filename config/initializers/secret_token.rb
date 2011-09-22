94b520be21862c6f1e8f3db1b144b4985af6e143
Brandon Cormany <brandoncormany@gmail.com>
2011-09-22 16:33:28 -0700

    First
    First commit after creating project in eclipse

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '921ba07c594337fdad76786ff2d98d17c8950ea89b47af4e2801f8a76db06a264ee32110cf0ace2b15004a1b3e28c07b8aa066d868bfcbb0a8d4c60c650ce7b4'
